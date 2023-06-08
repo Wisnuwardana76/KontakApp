069a6147f935c8d0c169bcaa43c846f93883bddb
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-06-08 10:37:19 +0700

    edit

--- app/src/main/java/com/example/catatan_pengeluaran/Kontakkelas.java
+++ app/src/main/java/com/example/catatan_pengeluaran/Kontakkelas.java
@@ -5,5 +5,46 @@ import java.io.Serializable;
 public class Kontakkelas implements Serializable
 {
     public String nama;
-    public String hpb
+    public String no_hp;
+    public String status;
+    public String color;
+
+    public Kontakkelas(String nama, String no_hp, String status, String color) {
+        this.nama = nama;
+        this.no_hp = no_hp;
+        this.status = status;
+        this.color = color;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNo_hp() {
+        return no_hp;
+    }
+
+    public void setNo_hp(String no_hp) {
+        this.no_hp = no_hp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
